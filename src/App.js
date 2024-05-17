325cabf3d30e4ad7d6a3d95e097573ee147a144e
NeelimaAitha <138878450+NeelimaAitha@users.noreply.github.com>
2024-05-17 12:59:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.unsplash.com/photos/random?count=10&client_id=O7-xcpW6nRhMMsx3meSpmzsR2NEOsM8Rn0toNL5DGs0'
+        );
+        setImages(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Search App</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="image-container">
+          {images.map((image) => (
+            <div className="image-card" key={image.id}>
+              <img src={image.urls.regular} alt={image.alt_description} />
+              <div className="image-details">
+                <p>{image.alt_description}</p>
+                <a
+                  href={image.links.html}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View on Unsplash
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
